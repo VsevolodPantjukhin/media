ecbfb1039d9cd13ae879222651e7db705b6455a6
haki_ainz <seva.pantiukhin@yandex.ru>
2024-01-09 15:11:18 +0400

    Wrapping up the Thunk

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
   return <div>UsersList</div>;
 };
 export default UsersList;
