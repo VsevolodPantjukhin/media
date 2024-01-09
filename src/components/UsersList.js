a992bf35e910429d21fa673d9f0d4c727a07e7a2
haki_ainz <seva.pantiukhin@yandex.ru>
2024-01-09 15:58:05 +0400

    Rendering a List of Users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
+import Skeleton from './Skeleton';
 
 const UsersList = () => {
   const dispatch = useDispatch();
@@ -13,13 +14,23 @@ const UsersList = () => {
   }, [dispatch]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Skeleton times={6} className="h-10 w-full" />;
   }
 
   if (error) {
     return <div>Error fetchin data</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedUsers}</div>;
 };
 export default UsersList;
