b4b853d13e0e544947787fb8df5c12ab9e345424
haki_ainz <seva.pantiukhin@yandex.ru>
2024-01-09 15:24:24 +0400

    Using Loading State

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,10 +4,22 @@ import { fetchUsers } from '../store';
 
 const UsersList = () => {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
-  return <div>UsersList</div>;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetchin data</div>;
+  }
+
+  return <div>{data.length}</div>;
 };
 export default UsersList;
